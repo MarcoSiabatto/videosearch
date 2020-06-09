d9a099b4b146d716a6a758ebc0da90e3a07f7898
MarcoSiabatto <57923265+MarcoSiabatto@users.noreply.github.com>
2020-06-09 13:51:07 -0500

    Updates

---
+++ precache-manifest.4b908c37777dc047be04978484e424ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11cdfa9413d471112e3e581809f8eae",
+    "url": "/videosearch/index.html"
+  },
+  {
+    "revision": "af2f20101d6e2a2a4900",
+    "url": "/videosearch/static/css/main.4965ec2a.chunk.css"
+  },
+  {
+    "revision": "a7f1454ebd63c01af9b4",
+    "url": "/videosearch/static/js/2.7ab2f618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videosearch/static/js/2.7ab2f618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2f20101d6e2a2a4900",
+    "url": "/videosearch/static/js/main.fc244455.chunk.js"
+  },
+  {
+    "revision": "8d7969a2cfde22e1158b",
+    "url": "/videosearch/static/js/runtime-main.3a17a941.js"
+  }
+]);
